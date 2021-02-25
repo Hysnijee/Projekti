93905fcd7b287a7b63f6a8f2639141c35b966d7b
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-02-25 22:50:40 +0100

    contact us file

---
+++ reusableCode/footer.php
@@ -0,0 +1,33 @@
+<hr id="hr-separate">>
+    <div class="footer">
+        <ul class="main-list">
+            <ul class="inner-list">
+                <li><img src="../images/oriflameLogo.png"></li>
+                <li>COMPANY LOGO</li>
+            </ul>
+            <ul class="inner-list">
+                <li>WEEBLY THEMES</li>
+                <li>PRE-SALE FAQS</li>
+                <li>SUBMIT A TICKET</li>
+            </ul>
+            <ul class="inner-list">
+                <li>WEEBLY THEMES</li>
+                <li>PRE-SALE FAQS</li>
+                <li>SUBMIT A TICKET</li>
+            </ul>
+            <ul class="inner-list">
+                <li>ABOUT US</li>
+                <li>CONTACT US</li>
+                <li>AFFILIATES</li>
+                <li>RESOURCES</li>
+            </ul>
+        </ul>
+    </div>
+    <div class="socials">
+        <a href="https://www.instagram.com/"><img src="../images/insta-logo.png"></a>
+        <a href="https://www.facebook.com/"><img src="../images/fb-logo.png"></a>
+        <a href="https://www.twitter.com/"><img src="../images/twitter-logo.png"></a>
+        <a href="https://www.youtube.com/"><img src="../images/yt-logo.png"></a>
+    </div>
+</body>
+</html>
---
+++ views/contactUs.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oriflame</title>
+    <link rel="stylesheet" href="../style/contactUs.css">
+</head>
+<body>
+    <div class="header"></div>
+    <div class="main">
+        <div class="box1">
+            <ul>
+                <li><h1>00</h1></li>
+                <li>Morbi libero ligula, molestie id pulvinar tempus, efficitur vitae ante. 
+                Integer at leo tincidunt, ultricies nibh vitae, mollis ex. Cras lorem leo, 
+                ullamcorper vitae nunc id, consectetur laoreet velit. 
+                Vivamus venenatis, ligula vitae consequat accumsan, purus justo varius mi, vitae dapibus neque lorem vitae turpis. 
+                Fusce finibus metus massa, quis efficitur ligula ultricies at. 
+                Aenean mi mauris, lacinia in venenatis nec, venenatis non nunc. 
+                Nam vel ipsum ac quam congue gravida. Nunc at est quis velit efficitur vestibulum. 
+                Cras molestie efficitur mauris, in varius tellus vehicula ut. 
+                Praesent molestie risus id neque mollis, et posuere lectus condimentum. 
+                Cras vitae nisi in ligula tempus faucibus ut commodo massa. </li>
+                <li><img src="../images/ceo.jpg"></li>
+            </ul>
+        </div>
+        <div class="box2">
+            <form>
+                <label>Email</label>
+                <input type="text" placeholder="Enter a valid email address">
+                <label>Name</label>
+                <input type="text" placeholder="Enter your name">
+                <label>Address</label>
+                <input type="text" placeholder="Enter your address">
+                <label>Message</label>
+                <input type="text" placeholder="Enter your message">
+                <input type="submit" value="Submit" id="submit">
+            </form>
+        </div>
+    </div>
+    <?php 
+        include('../reusableCode/footer.php');
+    ?>
