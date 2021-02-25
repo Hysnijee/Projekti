101fa4bc9f0acd85dba0b43a9921e050d47f6b80
Hysnijee <73667060+Hysnijee@users.noreply.github.com>
2021-02-25 21:45:18 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oriflame</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+    </div>
+    <div class="main">
+        <div class="first-main">
+            <ul>
+                <li id="first-li"><img src="images/prod1.jpg"></li>
+                <li id="second-li"><img src="images/prod2.jpg"><div id="concept">ACCESORIES</div></li>
+                <li id="third-li"><img src="images/prod3.jpg"></li>
+                <li id="fourth-li">
+                    <h1>Lorem Ipsum</h1>
+                    <p>
+                        It is a long established fact that a reader 
+                        will be distracted by the readable content 
+                        of a page when looking at its layout. 
+                        The point of using Lorem Ipsum is that it has 
+                        a more-or-less normal distribution of letters, 
+                        as opposed to using 'Content here, content here', 
+                        making it look like readable English. 
+                        Many desktop publishing packages and web page 
+                        editors now use Lorem Ipsum as their default model 
+                        text, and a search for 'lorem ipsum' will uncover 
+                        many web sites still in their infancy.
+                    </p>
+                </li>
+            </ul>
+        </div>
+        <div class="second-main">
+            <ul>
+                <li><img src="images/prod16.jpg"></li>
+                <li><img src="images/prod17.jpg"></li>
+                <li><img src="images/prod7.jpg"></li>
+                <li><img src="images/prod8.jpg"></li>
+                <li><img src="images/prod9.jpg"></li>
+                <li><img src="images/prod10.jpg"></li>
+                <li><img src="images/prod13.jpg"></li>
+                <li><img src="images/prod15.jpg"></li>
+                <li><img src="images/prod11.jpg"></li>
+                <li><img src="images/prod9.jpg"></li>
+                <li><img src="images/prod14.jpg"></li>
+            </ul>
+        </div>
+        <div class="third-main">
+            <div class="box">
+                <div class="inner-box-first">
+                    <img src="images/oriflameLogo.png">
+                </div> 
+                <div class="inner-box-second">
+                    <hr>
+                    <p>Choose your</p><br>
+                    <div id="concept2">COSMETICS</div>
+                </div>
+                <div class="inner-box-third">
+                    <p>Amet nulla facilisi morbi tempus iaculis urna. 
+                        Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. 
+                        Sodales ut eu sem integer vitae justo eget magna. 
+                        Ornare suspendisse sed nisi lacus. 
+                        Nunc sed velit dignissim sodales ut eu. </p>
+                </div>
+            </div>
+        </div>
+        <div class="fourth-main">
+            <div class="f1">
+                <h1>01<h1>
+            </div>
+            <div class="f2">
+                <p>Magna eget est lorem ipsum dolor sit amet consectetur. 
+                    Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. 
+                    Nibh nisl condimentum id venenatis a condimentum vitae sapien 
+                    pellentesque. Interdum velit laoreet id donec ultrices. 
+                    Euismod in pellentesque massa placerat duis. 
+                    Urna condimentum mattis pellentesque id. 
+                    Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. </p>
+                <a href="learnMore.php"><input type="submit" value="learn more" id ='button'></a>
+            </div>
+        </div>
+        <div class="fifth-main">
+            <div class="f2">
+                <p>Magna eget est lorem ipsum dolor sit amet consectetur. 
+                    Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. 
+                    Nibh nisl condimentum id venenatis a condimentum vitae sapien 
+                    pellentesque. Interdum velit laoreet id donec ultrices. 
+                    Euismod in pellentesque massa placerat duis. 
+                    Urna condimentum mattis pellentesque id. 
+                    Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. </p>
+                <a href="learnMore.php"><input type="submit" value="learn more" id ='button'></a>
+            </div>
+            <div class="f1">
+                <h1>02<h1>
+            </div>
+        </div>
+        <div class="sixth-main">
+        <div class="f1">
+                <h1>03<h1>
+            </div>
+            <div class="f2">
+                <p>Magna eget est lorem ipsum dolor sit amet consectetur. 
+                    Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. 
+                    Nibh nisl condimentum id venenatis a condimentum vitae sapien 
+                    pellentesque. Interdum velit laoreet id donec ultrices. 
+                    Euismod in pellentesque massa placerat duis. 
+                    Urna condimentum mattis pellentesque id. 
+                    Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. </p>
+                <a href="learnMore.php"><input type="submit" value="learn more" id ='button'></a>
+            </div>
+        </div>
+    </div>
+    <hr id="hr-separate">
+    <div class="footer">
+        <ul class="main-list">
+            <ul class="inner-list">
+                <li><img src="images/oriflameLogo.png"></li>
+                <li>COMPANY LOGO</li>
+            </ul>
+            <ul class="inner-list">
+                <li>WEEBLY THEMES</li>
+                <li>PRE-SALE FAQS</li>
+                <li>SUBMIT A TICKET</li>
+            </ul>
+            <ul class="inner-list">
+                <li>WEEBLY THEMES</li>
+                <li>PRE-SALE FAQS</li>
+                <li>SUBMIT A TICKET</li>
+            </ul>
+            <ul class="inner-list">
+                <li>ABOUT US</li>
+                <li>CONTACT US</li>
+                <li>AFFILIATES</li>
+                <li>RESOURCES</li>
+            </ul>
+        </ul>
+    </div>
+    <div class="socials">
+        <a href="https://www.instagram.com/"><img src="images/insta-logo.png"></a>
+        <a href="https://www.facebook.com/"><img src="images/fb-logo.png"></a>
+        <a href="https://www.twitter.com/"><img src="images/twitter-logo.png"></a>
+        <a href="https://www.youtube.com/"><img src="images/yt-logo.png"></a>
+    </div>
+</body>
+</html>
