f96d3e973bd10c961d5d346628c634c233918e3e
Albiona Berisha <albionaberish@gmail.com>
2021-03-07 15:06:07 +0100

    Ndryshime ne Validim te checkBox

--- js/login.js
+++ js/login.js
@@ -1,24 +1,28 @@
-const LogInbtn = document.getElementById('login');
-LogInbtn.addEventListener("click", (event)=>{
-        event.preventDefault();
-        const username = document.getElementById('userN');
-        const fjalkalimi = document.getElementById('pass');
+function ValidimiLogin(){
+    let LogInbtn = document.getElementById('login');
 
-        if (username.value == "") {
-            alert("Ju duhet te shtoni username-in.");
-            return false;
-        }
+            const username = document.getElementById('userN');
+            const fjalkalimi = document.getElementById('pass');
+            const rememberMe = document.getElementById('rememberme');
 
-        else if (fjalkalimi.value == "") {
-            alert("Ju duhet te shtoni Fjalkalimin.");
-            return false;
-        }
-        else if (fjalkalimi.length < 7) {
-            alert("Fjalekalimi juaj duhet te permbaje se paku 8 karaktere.");
-            return false;
-        }
-        else{
-            alert("Ju jeni kycur me sukses!");
-            return true;
-        }
-});
+            if (username.value == "") {
+                alert("Ju duhet te shtoni username-in.");
+                return false;
+            }
+            else if (fjalkalimi.value == "") {
+                alert("Ju duhet te shtoni Fjalkalimin.");
+                return false;
+            }
+            else if (fjalkalimi.length < 7) {
+                alert("Fjalekalimi juaj duhet te permbaje se paku 8 karaktere.");
+                return false;
+            }
+            else if(rememberMe.checked==true){
+                alert("Te dhenat tuaja jane ruajtur dhe ju jeni kycur me sukses!");
+            }
+            else{
+                alert("Ju jeni kycur me sukses!");
+                return true;
+            }
+       
+}
