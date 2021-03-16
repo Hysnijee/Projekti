5f54343d21ab218495a79572e7f45845985b1cc5
Albiona Berisha <albionaberish@gmail.com>
2021-03-16 11:40:41 +0100

    Ndryshime

--- views/Cosmetic.php
+++ views/Cosmetic.php
@@ -1,3 +1,7 @@
+<?php
+    include_once '../components/product.php';
+    require_once '../components/productMapper.php';
+?>
 <!DOCTYPE html>
     <head>
         <title>Cosmetics</title>
@@ -25,469 +29,77 @@
             </div>
             
             <div id="BathBody">
-                <h4>Bath & Body </h4>
+            <h4>Bath & Body</h4>
                 <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/bb1.png">
-                    <p>Body Lotion FAIRNESS</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 8.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb2.jpeg">
-                    <p>Lotion FAIRNESS</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 6.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb3.jpg">
-                    <p>Bubble Bath</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 4.50 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/bb4.png">
-                    <p>Feminelle Special Care</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 5.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb5.png">
-                    <p>Bubble Bath Sweetheart</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb6.jpg">
-                    <p>Body & Mind </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/bb7.jpg">
-                    <p>Silk Beauty - Body Wash</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 5.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb8.jpg">
-                    <p>Milk & Honey - Body Wash </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 5.60 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb9.jpg">
-                    <p>Tenderly - Body Spray </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 4.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/bb10.jpg">
-                    <p>Coconut & Ricemilk Shampoo </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 8.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb11.jpg">
-                    <p>Milk Honey Gold</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 7.90 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/bb12.jpg">
-                    <p>Love Nature - Shower gel</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 7.00 $</p>
-                </div>
+                    
+                    <?php 
+                    $object = new ProductMapper();
+                    $object->getProductsByLowerCategory('Bath & Body');
+                    ?>
+                    
                 </div>
             </div>
 
-           
-
             <div id="LipGloss">
-                <h4>Lip Gloss</h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/lg1.Jpeg">
-                    <p>Nude Lip Gloss</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lg2.jpg">
-                    <p>Pink Lip Gloss</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lg3.Jpeg">
-                    <p>Purple Lip Gloss</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
+                <h4>Lip Gloss</h4> 
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Lip Gloss');
+                ?>
             </div>
        
 
 
             <div id="Lip">
                 <h4>Lip </h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/lip1.Jpeg">
-                    <p>Open Pink Lip </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lip2.jpg">
-                    <p>Pink Lip </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lip3.jpg">
-                    <p>Nude Lip</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/lip4.jpg">
-                    <p>Purple Lip</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lip5.Jpeg">
-                    <p>Red Lip</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lip6.jpg">
-                    <p>Nude Lip</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 3.00 $</p>
-                </div>
-                </div>
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Lip');
+                ?>
             </div>
 
 
             <div id="Foundation">
                 <h4>Foundation</h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/f1.jpg">
-                    <p>The One Foundation - Everlasting</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 12.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/f6.jpg">
-                    <p>Matte Control Foundation</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 24.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/f3.Jpeg">
-                    <p>The One Foundation - Everlasting Extreme</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 15.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/f4.jpg">
-                    <p>Illuskin Foundation and Concealer</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 28.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/f5.Jpeg">
-                    <p>One Color Foundation</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 8.00 $</p>
-                </div>
-
-                </div>
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Foundation');
+                ?>
             </div>
-
-
 
             <div id="LashMascara">
                 <h4>Lash Mascara </h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/lash1.Jpeg">
-                    <p>Waterproof Mascara</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 8.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lash2.jpg">
-                    <p>FatLash Mascara</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 6.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lash3.Jpeg">
-                    <p>Wonder Lash XXL</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 7.50 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/lash4.Jpeg">
-                    <p>5-in-1 Wonder Lash Mascara</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 7.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lash5.jpg">
-                    <p>Long Lash Mascara</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lash6.jpg">
-                    <p>Wonder Lash Mascara </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/lash7.Jpeg">
-                    <p>Double Effect Mascara</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 12.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/lash8.jpg">
-                    <p>Theme Douse Mascara </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 5.60 $</p>
-                </div>
-                </div>
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Lash Mascara');
+                ?>
             </div>
 
 
 
             <div id="Parfume">
                 <h4>Parfume</h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/p1.jpg">
-                    <p>Macnetista</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 25.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/p2.jpg">
-                    <p>Paradise</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 40.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/p3.jpg">
-                    <p>Mysterial OUD</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 19.50 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/p4.jpg">
-                    <p>Infinita</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 34.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/p5.jpg">
-                    <p>Amber Elixer</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 43.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/p6.jpg">
-                    <p>Eclat Mon Parfum </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 27.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/p7.jpg">
-                    <p>Possess</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 31.00 $</p>
-                </div> 
-                </div>
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Parfume');
+                ?>
             </div>
 
 
             <div id="Powder">
                 <h4>Powder</h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/powder1.Jpeg">
-                    <p>ColourBox Face Powder 3</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 12.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/powder2.jpg">
-                    <p>Powder</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 8.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/powder3.jpg">
-                    <p>ColourBox Face Powder 1</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 12.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/powder4.webp">
-                    <p>Colour Powder </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 24.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/powder5.webp">
-                    <p>Powder 2</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 12.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/powder6.webp">
-                    <p>Colour Powder </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 24.00 $</p>
-                </div>
-                </div>
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Powder');
+                ?>
             </div>
 
             <div id="Organic">
                 <h4>Organic Products</h4>
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/organic1.Jpeg">
-                    <p>Hydratin face cream - Normal Skin</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 8.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/organic2.Jpeg">
-                    <p>Nourishing face cream - Dry Skin</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 6.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/organic3.jpg">
-                    <p>Purifying face wash</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 4.50 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/organic4.Jpeg">
-                    <p>Blackhead Clearing Mask</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 5.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/organic5.jpeg">
-                    <p>Cleansing Gel - Oily Skin</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/organic6.jpg">
-                    <p>Protecting Face Wash </p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-                </div>
-
-                <div id="Rreshti">
-                <div id="Produkti">
-                    <img src="../images/organic7.Jpeg">
-                    <p>Purifying Face Oil</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 9.00 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/organic8.jpg">
-                    <p>Cracked Heel Repair & Smooth</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 7.60 $</p>
-                </div>
-
-                <div id="Produkti">
-                    <img src="../images/organic9.jpg">
-                    <p>Silk Baeuty - Hand Cream</p>
-                    <p id="p2"> Cosmetics</p>
-                    <p>çmimi: 4.00 $</p>
-                </div>
-                </div>
+                <?php 
+                $object = new ProductMapper();
+                $object->getProductsByLowerCategory('Organic Products');
+                ?>
             </div>
 
         </main>
