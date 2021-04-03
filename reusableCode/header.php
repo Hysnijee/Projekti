de329695216db6ee30333a739e904e8855c99a6a
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-04-03 15:14:01 +0200

    Catalog page done

--- reusableCode/header.php
+++ reusableCode/header.php
@@ -2,13 +2,12 @@
     <div class="header">
         <ul>
             <li><a href="homepage.php">Home</a></li>
+            <li><a href="catalogs.php">Catalog</a></li>
             <li><a href="contactUs.php">Contact Us</a></li>
             <?php
                 if(isset($_SESSION['role']) && $_SESSION['role'] == '1') {
                 ?>
-                <a href="../views/dashboard.php">
-                <li>Dashboard</li>
-                </a>
+                <li><a href="../views/dashboard.php">Dashboard</a></li>
             <?php
             }
             ?>
---
+++ views/catalogs.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oriflame</title>
+    <link rel="stylesheet" href="../style/catalogs.css">
+</head>
+    <?php
+        include('../reusableCode/header.php');
+    ?>
+    <div class="main">
+        <ul class="inn-list">
+            <li><img src="../images/catalog/c1.jpg"><br><a href="../pdf/./Oriflame Catalog APRIL 2021 hd.pdf" target="blank">VIEW</a></li>
+            <li><img src="../images/catalog/c2.jpg"><br><a href="../pdf/./Oriflame Catalog MARCH 2021.pdf" target="blank">VIEW</a></li>
+            <li><img src="../images/catalog/c3.jpg"><br><a href="../pdf/./Oriflame Catalogue January 2021.pdf" target="blank">VIEW</a></li>
+        </ul>
+        <ul class="inn-list">
+            <li><img src="../images/catalog/c4.jpg"><br><a href="../pdf/./Oriflame catalogue november 2020.pdf" target="blank">VIEW</a></li>
+            <li><img src="../images/catalog/c5.jpg"><br><a href="../pdf/./Oriflame Catalogue september 2020.pdf" target="blank">VIEW</a></li>
+            <li><img src="../images/catalog/c6.jpg"><br><a href="../pdf/./Oriflame catalogue August 2020.pdf" target="blank">VIEW</a></li>
+        </ul>
+        <ul class="inn-list">
+            <li><img src="../images/catalog/c7.jpg"><br><a href="../pdf/./Oriflame catalogue July 2020.pdf" target="blank">VIEW</a></li>
+        </ul>
+    </div>
+    <?php
+        include('../reusableCode/footer.php');
+    ?>
+</body>
+</html>
