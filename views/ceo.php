77faff9a4606183101b159f7cc0ef92fc730e450
albionaberisha20 <77298717+albionaberisha20@users.noreply.github.com>
2021-04-08 20:19:55 +0200

    Ceo.php all done!

--- views/ceo.php
+++ views/ceo.php
@@ -8,7 +8,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
 </head>
-<body class="main">
+<body>
+    <?php
+        include('../reusableCode/header.php');
+    ?>
+    <div class="main">
     <div id="ceoAlbiona">
     <div class="card">
         <img src="../images/ceoAlbiona.PNG" alt="Albiona" style="width:100%">
@@ -18,13 +22,10 @@
         <div style="margin: 24px 0;">
             <a href="https://www.instagram.com/albionaberish/"><i class="fa fa-instagram"></i></a> 
             <a href="https://twitter.com/albionaberish"><i class="fa fa-twitter"></i></a>  
-            <a href="#"><i class="fa fa-linkedin"></i></a>  
+            <a href="https://www.linkedin.com/in/albiona-berisha-a0a42920b/"><i class="fa fa-linkedin"></i></a>  
             <a href="https://www.facebook.com/albiona.berisha.547/"><i class="fa fa-facebook"></i></a> 
         </div>
-        <p><button type="button" class="collapsible">Contact</button></p>
-        <div class="content">
-            <p>+38345123456</p>
-        </div>
+        <p><button type="button">Contact</button></p>
     </div>
     </div>
 
@@ -37,31 +38,40 @@
             <div style="margin: 24px 0;">
                 <a href="https://www.instagram.com/hysnije_z/"><i class="fa fa-instagram"></i></a> 
                 <a href="https://twitter.com/HysnijeZ"><i class="fa fa-twitter"></i></a>  
-                <a href="#"><i class="fa fa-linkedin"></i></a>  
+                <a href="https://www.linkedin.com/in/hysnije-zllanoga-6881a01a7/"><i class="fa fa-linkedin"></i></a>  
                 <a href="https://www.facebook.com/hysnije.zllanoga.7"><i class="fa fa-facebook"></i></a> 
             </div>
-            <p><button type="button" class="collapsible">Contact</button></p>
-            <div class="content">
-                <p>+38349123456</p>
+            <p><button type="button">Contact</button></p>
+        </div>
+        </div>
+    </div>
+    <div class="about">
+        <h1>About our CEOs</h1>
+        <p id="ceobio">
+            <b><i>Albiona Berisha</i></b>, e lindur me 15.03.2001 ne Prishtine. Shkollimin e ulet dhe te mesem te ulet e kreu ne 
+            fshatin Dejne, Komuna e Rahovecit. Ne vitin 2019 perfundoj shkollimin e mesem ne gjimnazin "Hajdar Dushi" ne Gjakove, dhe aktualisht
+            eshte studente e nivelit bachelor per Shkenca Kompjuterike dhe Inxhinieri ne Universitetin per Biznes dhe Teknologji (UBT).
+        </p>
+        <p id="ceobio">
+            <b><i>Hysnije Zllanoga</i></b>, e lindur me 03.06.2001 ne Rahovec. Shkollimin e ulet dhe te mesem te ulet e kreu ne 
+            Rahovec, Komuna e Rahovecit. Ne vitin 2019 perfundoj shkollimin e mesem ne gjimnazin "Xhelal Hajda - Toni" ne Rahovec, dhe aktualisht
+            eshte studente e nivelit bachelor per Shkenca Kompjuterike dhe Inxhinieri ne Universitetin per Biznes dhe Teknologji (UBT).
+        </p>
+    </div>
+    <div class="contactCeos">
+            <div id="ceoAlbiona">
+                <ul id="ulc"> 
+                <li><img src="../images/ceoEmail.PNG"><span style="padding-left:10px;">Email:</span> albionaberish@hmail.com</li></br>
+                <li><img src="../images/ceoTel.png"><span style="padding-left:10px;">Numri</span> tel.: +383845282594</li></ul>
+            </div></br>
+            <div id="ceoHysnije">
+                <ul id="ulc"> 
+                <li><img src="../images/ceoEmail.PNG"><span style="padding-left:10px;">Email:</span> hysnijee.zllanoga@hmail.com</li></br>
+                <li><img src="../images/ceoTel.png"><span style="padding-left:10px;">Numri</span> tel.: +383849604201</li></ul>
             </div>
-        </div>
-        </div>
-
-        <script>
-            var coll = document.getElementsByClassName("collapsible");
-            var i;
-            
-            for (i = 0; i < coll.length; i++) {
-              coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                if (content.style.display == "block") {
-                  content.style.display = "none";
-                } else {
-                  content.style.display = "block";
-                }
-              });
-            }
-        </script>
+    </div>
+        <?php 
+            include('../reusableCode/footer.php');
+        ?>
 </body>
 </html>
