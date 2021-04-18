c664d2715b23a60eb632fb89b53a0ee11c78ad4c
albionaberisha20 <77298717+albionaberisha20@users.noreply.github.com>
2021-04-18 17:59:32 +0200

    .

--- js/login.js
+++ js/login.js
@@ -1,23 +1,23 @@
 function ValidimiLogin(){
     let loginbtn = document.getElementById('login');
+    
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
 
-            const username = document.getElementById('username');
-            const password = document.getElementById('password');
-
-            if (username.value == "") {
-                alert("Ju duhet te shtoni username-in.");
-                return false;
-            }
-            else if (password.value == "") {
-                alert("Ju duhet te shtoni Fjalkalimin.");
-                return false;
-            }
-            else if (password.length < 7) {
-                alert("Fjalekalimi juaj duhet te permbaje se paku 8 karaktere.");
-                return false;
-            }
-            else{
-                return true;
-            }
+    if (username.value == "") {
+        alert("Ju duhet te shtoni username-in.");
+        return false;
+    }
+    else if (password.value == "") {
+        alert("Ju duhet te shtoni Fjalkalimin.");
+        return false;
+    }
+    else if (password.length < 7) {
+        alert("Fjalekalimi juaj duhet te permbaje se paku 8 karaktere.");
+        return false;
+    }
+    else{
+        return true;
+    }
        
 }
