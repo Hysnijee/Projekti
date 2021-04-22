adef94354fa5cb0d238f254f28fa33317a5b9c19
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-04-22 12:13:06 +0200

    changes

--- components/registerVerify.php
+++ components/registerVerify.php
@@ -34,6 +34,26 @@ class Registration{
         $this->city=$formData['register-city'];
     }
 
+    /*public function insertUser(\SimpleUser $user){
+        $query = "insert into user (name, lastname, username, email, password, role) values (:name, :lastname, :username, :email, :pass,:role)";
+        $statement = $this->conn->prepare($query);
+        
+        $name = $user->getName();
+        $lastname = $user->getLastName();
+        $username = $user->getUsername();
+        $email = $user->getEmail();
+        $password = password_hash($user->getPassword(), PASSWORD_DEFAULT);
+        $role = $user->getRole();
+
+        $statement->bindParam(":name", $name);
+        $statement->bindParam(":lastname", $lastname);
+        $statement->bindParam(":username", $username);
+        $statement->bindParam(":email", $email);
+        $statement->bindParam(":password", $password);
+        $statement->bindParam(":role", $role);
+        $statement->execute();
+    }*/
+
     public function insertData(){
         $user = new SimpleUser($this->username, $this->password, 0, $this->email, $this->name, $this->lastname, $this->city, $this->address, $this->ccNo);
         $mapper = new UserMapper();
